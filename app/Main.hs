25ca489755818b2c16cd960e7eeb02ca995ec345
Your Name <you@example.com>
2017-05-12 13:18:00 +0000

    Basic working remote API! (ish)

--- app/Main.hs
+++ app/Main.hs
@@ -43,8 +43,11 @@ master backend slaves = do
   -- log list of slaves to stderr
   liftIO . hPutStrLn stderr  $ "MASTER :: Found slaves: " ++ show slaves
   liftIO . hPutStrLn stderr $ "MASTER :: Total slaves : " ++ (show $ length slaves)
-  pids <- sequence $ map (flip spawn $ $(mkClosure 'listenAndPrint) ()) slaves
-  sequence $ map (flip send (Msg "hello")) pids
+--  pids <- sequence $ map (flip spawn $ $(mkClosure 'listenAndPrint) ()) slaves
+  -- sequence $ map (flip send (Msg "hello")) pids
+  -- Test spawn
+  say "Spawning local"
+  run test
   -- Terminate all the slaves
   terminateAllSlaves backend
 
@@ -61,10 +64,16 @@ main = do
       backend <- initializeBackend host port newRemoteTable
       startSlave backend
 
-data RemoteAlg a = Transfer NodeId a deriving (Eq, Show)
+
+type Node = NodeId
+data RemoteAlg a = Transfer NodeId a
+                 | MyId (String -> a)
+                 | RemotePrint String a
 
 instance Functor RemoteAlg where
   fmap f (Transfer nid x) = Transfer nid (f x)
+  fmap f (RemotePrint s x) = RemotePrint s (f x)
+  fmap f (MyId g) = MyId (f . g)
 
 type Remote = Free RemoteAlg
 
@@ -73,3 +82,27 @@ data Ret a = Value a | Gone deriving (Eq, Show)
 run :: Remote a -> Process (Ret a)
 run (Pure x) = pure (Value x)
 run (Free (Transfer _ rest)) = spawnLocal (run rest >> return ()) >> pure Gone
+run (Free (MyId f)) = do
+  pid <- getSelfPid
+  run (f $ show pid)
+run (Free (RemotePrint s rest)) = do
+  pid <- getSelfPid
+  say ("\n[" ++ show pid ++ "] " ++ s) >> run rest
+
+transfer :: Node -> Remote ()
+transfer _ = liftF $ Transfer undefined ()
+
+display :: String -> Remote ()
+display s = liftF $ RemotePrint s ()
+
+locid :: Remote String
+locid = liftF $ MyId id
+
+test :: Remote ()
+test = do
+  pid <- locid
+  transfer undefined
+  display pid
+  pid <- locid
+  display pid
+  return ()
